a4135e6e27b118a92f30093d19990364b9b8cea9
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-07-06 22:35:40 -0500

    create the linked_list.rb file and the LinkedList class)

---
+++ lib/linked_list.rb
@@ -0,0 +1,12 @@
+require "./lib/node"
+
+class LinkedList
+  attr_reader :head
+
+  def initialize(head = nil)
+    @head = head
+  end
+
+  # def append method
+  # its going to add an attribute to the initialize???
+end
